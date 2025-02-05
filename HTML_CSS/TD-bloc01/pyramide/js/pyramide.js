96cb8514aa0f4618c829b0cd72315765494e0c85
Patebolognaise <79859430+Patebolognaise@users.noreply.github.com>
2025-02-05 19:45:17 +0100

    commente pyramide.js

--- HTML_CSS/TD-bloc01/pyramide/js/pyramide.js
+++ HTML_CSS/TD-bloc01/pyramide/js/pyramide.js
@@ -1,25 +1,24 @@
 "use strict";
-
+//declaration balises utilisées
 const input = document.getElementById("input");
 const bouton = document.getElementById("bouton");
 const Resultat = document.getElementById("Resultat");
 
-bouton.addEventListener("click", () => {
-
+bouton.addEventListener("click", () => { //quand le bouton est cliqué:
     let nbEtages = parseInt(input.value, 10); // Atribue nbEtages à l'entré. Si l'entré n'est pas un nombre, atribue "NaN" (Not a Number)
 
     // Vérification si l'entré ne possaide bien que des chifres
     if (isNaN(nbEtages) || nbEtages <= 0) {
         alert("Veuillez entrer un nombre valide d'étages.");
-        Resultat.classList.remove("show");
+        Resultat.classList.remove("show"); //cache les resultats
     }
 
     // Vérification du nombre d'étages (maximum 75)
     else if (nbEtages > 75) {
         alert("Le nombre d'étages ne peut pas dépasser 75.");
-        Resultat.classList.remove("show");
+        Resultat.classList.remove("show"); //cache les resultats
     }
-
+        
     else {
         Resultat.innerHTML = "<h2>Résultat</h2>"; // Vide la div
         
@@ -33,7 +32,6 @@ bouton.addEventListener("click", () => {
             Paragraphe.textContent = ligne;
             Resultat.appendChild(Paragraphe);
         }
-
         // Ajoute la classe "show" pour afficher avec animation
         Resultat.classList.add("show");
     }
