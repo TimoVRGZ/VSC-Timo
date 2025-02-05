64dae11069f7a106014a395ba88bab87d9f76854
TimoVRGZ <timo.vergezac@monlycee.net>
2025-02-05 11:43:47 +0100

    Update La suite de Syracuse_Timo_Benjamin.py
    ajout de commentaire et espace

--- python/La suite de Syracuse_Timo_Benjamin.py
+++ python/La suite de Syracuse_Timo_Benjamin.py
@@ -1,3 +1,8 @@
+# La suite Syracuse est une suite qui prend un nombre de depare. Si ce nombre est paire, il est diviser par 2 (/2). Si il est impaire, il est multiplier par 3 puis est additioner avec 1  (*3+1). Le nouveau nombre subit la meme chose, ainsi de suite jus'qu'à arriver à 1.
+# Ce programme permet de connaitre le nombre de depare le plus petit avec le quel on ateint x étapes. (ici: 30)
+
+# Fonctions:
+# retourne le prochain nombre dans la suite Syracuse
 def suivant(n):
     if (n % 2) == 0:
         return (n / 2)
@@ -5,14 +10,15 @@ def suivant(n):
     else:
         return (n * 3 + 1)
 
+# retourne une liste contenant toutes les étapes jusqu'à arriver à 1
 def trajet(n):
     chaine = []
     while n != 1:
         n = suivant(n)
         chaine.append(n)
-        
     return chaine
 
+# retourne le plus grand nombre du trajet de la suite
 def max(n):
     resultat = trajet(n)[0]
     for i in range (1, (len(trajet(n)))):
